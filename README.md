86debd5539f399df885ed8964de84ca820e94f31
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-21 11:08:09 -0300

    Create Spring Project with Eureka Server dependency

---
+++ README.md
@@ -0,0 +1,1 @@
+# Microstamp Service Registry
---
+++ src/main/java/service/registry/MicrostampServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package service.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrostampServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrostampServiceRegistryApplication.class, args);
+	}
+
+}
