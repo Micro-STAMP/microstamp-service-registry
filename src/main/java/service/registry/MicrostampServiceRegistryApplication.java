56dbaf50165c598c1967a6c2533336c79edafffd
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-07-22 18:57:48 -0300

    Enable Eureka server and disable it client feature

--- src/main/java/service/registry/MicrostampServiceRegistryApplication.java
+++ src/main/java/service/registry/MicrostampServiceRegistryApplication.java
@@ -2,8 +2,14 @@ package service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * The @EnableEurekaServer annotation transforms our SpringBoot application
+ * into a Eureka Server-based Service Registry.
+ */
 @SpringBootApplication
+@EnableEurekaServer
 public class MicrostampServiceRegistryApplication {
 
 	public static void main(String[] args) {
